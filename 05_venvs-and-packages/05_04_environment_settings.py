ccca4378af6eaf231be75061da5734209e8a1128
Tabitha <tabitha.r.mccracken@gmail.com>
2021-12-20 21:26:27 -0700

    Virtual Environments

--- 05_venvs-and-packages/05_04_environment_settings.py
+++ 05_venvs-and-packages/05_04_environment_settings.py
@@ -6,3 +6,9 @@
 # 
 # Then write the necessary code to access and print the values of these
 # two environment variables in this script.
+import os
+first_secret = os.environ['ENVIRONMENT']
+second_secret = os.environ['SECRET']
+
+print(first_secret)
+print(second_secret)
