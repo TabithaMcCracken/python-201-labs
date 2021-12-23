16351221ebe00a95dd467d710fe0ce9f902295c2
Tabitha <tabitha.r.mccracken@gmail.com>
2021-12-23 09:37:56 -0700

    Cleaning Venvs

--- 05_venvs-and-packages/Practice/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/tabithamccracken/Documents/codingnomads/python-201/05_venvs-and-packages/Practice/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- 05_venvs-and-packages/Practice2/venv2/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/tabithamccracken/Documents/codingnomads/python-201/05_venvs-and-packages/Practice2/venv2/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
