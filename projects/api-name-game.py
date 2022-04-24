415e84469abae9afcc2e991601de58ac73523fb2
Tabitha <tabitha.r.mccracken@gmail.com>
2022-04-23 20:10:51 -0600

    3 new

--- projects/api-name-game.py
+++ projects/api-name-game.py
@@ -1,1 +1,16 @@
 # Add an API call to your CLI game that assigns a name to your player.
+
+import requests
+
+name_len = 0
+while not (name_len >=2 and name_len <=40):
+    player_name = str(input("Please enter your name: \n"))
+    name_len = len(player_name)
+
+URL = f"https://uzby.com/api.php?min={name_len}&max={name_len}"
+
+response = requests.get(URL)
+new_name = response.text
+
+print(f"Welcome, {new_name}! In this game, your new name will be: {new_name}")
+print(" Enjoy the game, we hope you make it out alive! \n")
--- projects/barking-shibes.py
+++ projects/barking-shibes.py
@@ -7,3 +7,34 @@
 # Write the code logic to make the API calls and assign the output to
 # `doged_quote` and `doge_image_url` respectively.
 # Then write the `html` string to a `.html` file and look at it in your browser.
+
+import requests
+from pathlib import Path
+
+# Get quote
+# Quotes from API http://quotes.stormconsultancy.co.uk/random.json
+quote_url = "http://quotes.stormconsultancy.co.uk/random.json"
+quote = requests.get(quote_url).json()["quote"]
+print(f"Here is the quote: {quote}\n")
+
+# Convert the quote to doge
+# Conversion API https://api.funtranslations.com/translate/doge.json
+doge_url = f"https://api.funtranslations.com/translate/doge.json?text={quote}"
+doge_quote = requests.get(doge_url).json()["contents"]["translated"]
+print(f"Here is quote converted to doge: \n {doge_quote}")
+
+# Get dog image
+# image from API http://shibe.online/api/shibes
+img_url = "http://shibe.online/api/shibes?count=1"
+doge_img = requests.get(img_url).json()[0]
+print(f"Here is the link to the image: {doge_img} \n")
+
+
+# create html structure
+html = f"<p>Here is the original quote: {quote}</p><img src='{doge_img}'><p>Here is the doge translation: {doge_quote}</p>"
+print(html)
+
+# # create a new file
+f = Path().home().joinpath("Documents").joinpath("codingnomads").joinpath("python-201-copy").joinpath("projects").joinpath("dogetranslator.html")
+# write to the file
+f.write_text(html)
