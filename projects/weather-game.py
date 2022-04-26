b970e881e3f5f4430ccc0287572b4033c3307f53
Tabitha <tabitha.r.mccracken@gmail.com>
2022-04-25 22:15:42 -0600

    weather2

--- projects/weather-game.py
+++ projects/weather-game.py
@@ -9,5 +9,5 @@ location_URL = f"https://www.metaweather.com/api/location/search/?query={city}"
 locaton_id = requests.get(location_URL).json()[0]["woeid"]
 
 weather_url = f"https://www.metaweather.com/api/location/{locaton_id}"
-weather_forecast = requests.get(weather_url).json()
-print(weather_forecast)
+weather_forecast = requests.get(weather_url).json()["consolidated_weather"][0]["weather_state_name"]
+print(f"The current weather there is: {weather_forecast}\n")
