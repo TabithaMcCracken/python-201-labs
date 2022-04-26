5ad36e4adc6b9261aaaaefa8f913cf4328716986
Tabitha <tabitha.r.mccracken@gmail.com>
2022-04-25 22:07:04 -0600

    weather

--- projects/weather-game.py
+++ projects/weather-game.py
@@ -1,3 +1,13 @@
 # Include the current weather into your game mechanics.
+# URL = "https://www.metaweather.com/api/"
 
-URL = "https://www.metaweather.com/api/"
+import requests
+
+city = input(str("What is the closest large city to you?"))
+
+location_URL = f"https://www.metaweather.com/api/location/search/?query={city}"
+locaton_id = requests.get(location_URL).json()[0]["woeid"]
+
+weather_url = f"https://www.metaweather.com/api/location/{locaton_id}"
+weather_forecast = requests.get(weather_url).json()
+print(weather_forecast)
