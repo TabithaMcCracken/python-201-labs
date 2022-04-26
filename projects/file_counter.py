2e60a3fab62935b77e1494f4c9e02c7eea5de828
Tabitha <tabitha.r.mccracken@gmail.com>
2022-04-25 22:26:30 -0600

    file

--- projects/file_counter.py
+++ projects/file_counter.py
@@ -1,1 +1,16 @@
 # Add the code for the file counter script that you wrote in the course.
+
+# Get tools
+import pathlib
+
+# Find the path to my desktop
+desktop = pathlib.Path('/Users/tabithamccracken/Desktop')
+
+# Create new list
+file_list = [] 
+
+for filepath in desktop.iterdir(): # Loop through each file
+    if filepath.suffix == '.jpg': # Filter files with .jpg
+        file_list.append(str(filepath.name)) #Add to file list
+
+print(file_list) # Print the file list
--- projects/my_enumerate.py
+++ projects/my_enumerate.py
@@ -2,6 +2,15 @@
 # Define a function called `my_enumerate()` that takes an iterable as input
 # and gives back both the element as well as its index position as an integer.
 
-def my_enumerate():  # add your arguments
-      # add your code implementation
-      pass
+# Get name to use as iterable
+name = input(str("What is your name?"))
+
+# Function to loop through iterable
+def my_enumerate(word):
+      index = 0 # Should I define index inside or outside of the function?
+      for letter in word:
+            print(index, letter)
+            index += 1
+
+# Call the function
+my_enumerate(name)
